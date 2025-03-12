d90d64b17e6003d366adcdbd529cebc287c5c0e9
Sanchen Zhang <zsckevin777@gmail.com>
2025-03-12 17:40:42 -0300

    feat: add manage family functions and pages

---
+++ src/app/(site)/family/[id]/page.tsx
@@ -0,0 +1,10 @@
+import FamilyDetail from '@/components/FamilyDetail'
+import React from 'react'
+
+const FamilyDetailPage= () => {
+  return (
+    <FamilyDetail />
+  )
+}
+
+export default FamilyDetailPage
