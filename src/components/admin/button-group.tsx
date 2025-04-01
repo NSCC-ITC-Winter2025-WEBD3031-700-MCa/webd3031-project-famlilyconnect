80ef2a5755373bcaa5c661ee998bd3a3e9850fd5
dyueskinner <dyueskinner@gmail.com>
2025-04-01 10:02:48 -0300

    feat(action-btn): added view page to view user details

--- src/components/admin/button-group.tsx
+++ src/components/admin/button-group.tsx
@@ -1,35 +1,21 @@
 import { Button } from "@/components/ui/button";
 import { useState, useEffect } from 'react'
+import { useRouter } from 'next/navigation'
 interface ButtonGroupProps {
   userId: string
 }
 
 export function ButtonGroup({ userId }: { userId: string}) {
-  const [user, setUser] = useState<{ name: string; email: string; id: string; role: string; } | null>(null)
-  
-  async function handleViewClick() {
-    try{
-      const res = await fetch(`/api/members/${userId}`)
-      const data = await res.json();
-      setUser(data)
-    }catch(error){
-      console.error("Error fetching user:", error)
+    const router = useRouter();
+
+    function handleView(){
+      router.push(`/admin/members/view/${userId}`)  
     }
-  }
   return (
     <div className="flex gap-1">
-      <Button onClick={handleViewClick} variant="outline" className="rounded-l-md bg-green-500">View</Button>
+      <Button onClick={handleView} variant="outline" className="rounded-l-md bg-green-500">View</Button>
       <Button variant="outline" className="-ml-px bg-blue-500">Edit</Button>
       <Button variant="outline" className="rounded-r-md -ml-px bg-red-500">Delete</Button>
-
-      {user && (
-        <div className="modal">
-          <h1>{user.name}</h1>
-          <p>{user.id}</p>
-          <p>{user.email}</p>
-          <p>{user.role}</p>
-        </div>
-      )}
     </div>
   );
 }
